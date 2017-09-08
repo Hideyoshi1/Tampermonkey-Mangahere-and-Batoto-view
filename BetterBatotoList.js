9cf79c213a5bfb73aac0a0e5a93db7a980cca21b
Iosyf <iosyf123456@abv.bg>
2017-09-08 21:28:15 +0200

    Update BetterBatotoList.js

--- BetterBatotoList.js
+++ BetterBatotoList.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        bato.to
+// @match        bato.to/*
 // @grant        none
 // ==/UserScript==
 
